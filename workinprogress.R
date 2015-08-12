defed3c80291d8b31cbc1c461eb2892e0ecf9e66
Jamie Yates <jamie.yates79@gmail.com>
2015-08-12 13:06:13 +0100

    Fixed to create function ... needs further testing.

--- workinprogress.R
+++ workinprogress.R
@@ -1,14 +1,15 @@
-# pollutantmean <- function(directory, pollutant, id = 1:332) {
+pollutantmean <- function(directory, pollutant, id = 1:332) {
   # File contains header
   
   
   
   ## fileid <- '020' # Hard loading file temporarily 
-  directory <- "specdata" # Hard loading the directory! This will be passed to function.
-  pollutiondata <- data.frame()
+  ## directory <- "specdata" # Hard loading the directory! This will be passed to function.
+  pollutiondata <- data.frame() # Initialises an empty data frame
   
   
-  for(i in 1:332) {
+  
+  for(i in id) {
     
     # Sets leading zeroes for filename based on i length
     
@@ -24,13 +25,15 @@
     
     fileid <- paste(leadingzeroes,i,sep="")
     filelocname <- paste(directory,'/', fileid,'.csv', sep="") # Builds the file string for the read.csv
-    ifiledata <- read.csv(filelocname, header =  TRUE, sep = "," )
+    ifiledata <- read.csv(filelocname, header =  TRUE, sep = "," ) # Reads the data from CSV in i to the ifiledata object
     
     pollutiondata <- rbind(ifiledata, pollutiondata) # Appends the ifiledata to pollutiondata 
     
     print(i)
     
+    
+    
     }
-  
-
+  print(mean(pollutiondata$sulfate, na.rm = TRUE))
+}
   
